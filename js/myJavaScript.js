bb4c6f5ca9c1e141303d22780f7f4f0b13fe7acb
Rui Li <lirui.jobs@gmail.com>
2019-09-22 00:52:08 +1000

    Hover effect for series link images

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -2,6 +2,11 @@
 
 $(document).ready(function(){
 
-	
-
-	   });
+	for(var i=1; i<=3; i++){
+		$('#series a.series'+i).hover(function(){
+			$('#series a.series'+i+' img').prop('src', 'img/series'+i+'_color.jpg')
+		}, function(){
+			$('#series a.series'+i+' img').prop('src', 'img/series'+i+'.jpg')
+		});		
+	}
+});
